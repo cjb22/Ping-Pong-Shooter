26ca7062f696074fc84262cd193197d76d6e1cf3
Christopher <cjb22@hw.ac.uk>
2019-06-26 03:28:13 -0800

    First commit

---
+++ Kinect.py
@@ -0,0 +1,17 @@
+import cv2 as cv
+import freenect
+
+
+print("Starting..")
+print("Using OpenCV version: " + cv.__version__)
+
+if __name__ == "__main__":
+    print("Hi")
+else:
+    print("Called from another program")
+
+
+
+
+
+print("Exiting..")
