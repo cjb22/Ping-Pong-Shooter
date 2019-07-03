ae3f55950dfe19c7fce4ad377d7b9f7f8dc4e81c
Christopher <cjb22@hw.ac.uk>
2019-07-03 06:09:15 -0800

    Kinect Depth Image And Canny Edge  First Steps

--- Kinect.py
+++ Kinect.py
@@ -1,17 +1,35 @@
-import cv2 as cv
+#!/usr/bin/env python
 import freenect
+import cv2
+import frame_convert2
+
+cv2.namedWindow('Depth')
+#cv2.namedWindow('Video')
+
+print('Press ESC in window to stop')
 
 
-print("Starting..")
-print("Using OpenCV version: " + cv.__version__)
-
-if __name__ == "__main__":
-    print("Hi")
-else:
-    print("Called from another program")
+def get_depth():
+    return frame_convert2.pretty_depth_cv(freenect.sync_get_depth()[0])
 
 
+def get_video():
+    return frame_convert2.video_cv(freenect.sync_get_video()[0])
 
+while 1:
+    imgray = get_depth()
+    #imgray = cv2.GaussianBlur(imgray, (3, 3), 0)
 
+    edges = cv2.Canny(imgray,100,200,7)
 
-print("Exiting..")
+    #Display the image
+    cv2.imshow('Depth', imgray)
+    cv2.imshow('Edges', edges)
+    #cv2.imshow('Video', get_video())
+    
+    if cv2.waitKey(10) == 27:
+        break
+
+# When everything done, release the capture
+#cap.release()
+cv2.destroyAllWindows()
